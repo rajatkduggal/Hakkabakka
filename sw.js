9ae05af9d53288d44f89a18a314fee6516fe9790
rajatkduggal <mail.marvelgroup@gmail.com>
2026-05-28 17:49:21 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,50 +1,21 @@
-const CACHE_NAME = 'hakkabakka-v1';
-const ASSETS = [
-  'index.html',
-  'manifest.json',
-  'logo.jpg',
-  'pizza.png',
-  'cheeschilli.png',
-  'hnychlyptato.png',
-  'nudels.png',
-  'momos.png',
-  'fries.png',
-  'springroll.png',
-  'pasta.png',
-  'waffle.png',
-  'tandoorichicken.png',
-  'roganjosh.png',
-  'tandoorickntikka.png',
-  'kabab.png',
-  'tandoorimutton.png',
-  'muttoncurry.png',
-  'panipuri.png',
-  'bhallachat.png',
-  'tikkichat.png',
-  'dosa.png',
-  'idli.png',
-  'kulhadtea.png',
-  'kulhadcofee.png',
-  'coffee.png',
-  'couplecoffee.png',
-  'sweetlassi.png',
-  'chach.png',
-  'mojitoo.png',
-  'coffeeshake.png',
-  'cold_drink.png',
-  'water.png'
-];
+const CACHE_NAME = 'hakkabakka-v3';
 
-// Install Service Worker
+// Install and Cache Everything Automatically
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './app-icon.jpg',
+        './logo.jpg'
+      ]);
     }).then(() => self.skipWaiting())
   );
 });
 
-// Activate Service Worker
+// Activate and Clear Old Cache
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
@@ -55,12 +26,11 @@ self.addEventListener('activate', (e) => {
           }
         })
       );
-    })
+    }).then(() => self.clients.claim())
   );
-  return self.clients.claim();
 });
 
-// Fetch Cache Data (Offline Sync)
+// Network First with Cache Fallback
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     fetch(e.request).catch(() => {
@@ -68,4 +38,3 @@ self.addEventListener('fetch', (e) => {
     })
   );
 });
-
