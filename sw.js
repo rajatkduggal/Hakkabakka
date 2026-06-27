a0ef5c6b646a6fbbc343d7d9332f6da339cb90b3
rajatkduggal <mail.marvelgroup@gmail.com>
2026-06-28 00:25:57 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
+const CACHE_NAME="hakkabakka-v3";
+
 self.addEventListener("install", function(event){
-    self.skipWaiting();
+self.skipWaiting();
 });
 
 self.addEventListener("fetch", function(event){});
