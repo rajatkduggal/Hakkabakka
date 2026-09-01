84fd5a9965779b6d61e755e1cccfe2183a414d94
rajatkduggal <mail.marvelgroup@gmail.com>
2026-09-01 13:51:25 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,27 @@
-const CACHE_NAME="hakkabakka-v3";
+const CACHE_NAME = 'hakkabakka-v5';
 
-self.addEventListener("install", function(event){
-self.skipWaiting();
+self.addEventListener('install', event => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", function(event){});
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key =>
+          key === CACHE_NAME ? null : caches.delete(key)
+        )
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
+});
